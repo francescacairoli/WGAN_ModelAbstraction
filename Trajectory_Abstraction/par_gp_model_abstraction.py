c06a35cfddf7303c02b2c477015fd47faa1da9c3
francescacairoli <francescacairoli@yahoo.it>
2021-04-13 09:35:24 +0200

    working version

--- Trajectory_Abstraction/par_gp_model_abstraction.py
+++ Trajectory_Abstraction/par_gp_model_abstraction.py
@@ -21,9 +21,7 @@ import pickle
 import matplotlib.pyplot as plt
 plt.rcParams.update({'font.size': 22})
 
-from torchsummary import summary
-
-from Dataset import *
+from ParamDataset import *
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--n_epochs", type=int, default=200, help="number of epochs of training")
@@ -36,14 +34,14 @@ parser.add_argument("--latent_dim", type=int, default=480, help="dimensionality 
 parser.add_argument("--n_critic", type=int, default=5, help="number of training steps for discriminator per iter")
 parser.add_argument("--sample_interval", type=int, default=400, help="interval betwen image samples")
 parser.add_argument("--traj_len", type=int, default=32, help="number of steps")
-parser.add_argument("--x_dim", type=int, default=2, help="number of channels of x")
+parser.add_argument("--x_dim", type=int, default=1, help="number of channels of x")
+parser.add_argument("--par_dim", type=int, default=1, help="number of channels of par")
 parser.add_argument("--model_name", type=str, default="eSIRS", help="name of the model")
 parser.add_argument("--species_labels", type=str, default=["S", "I"], help="list of species names")
 parser.add_argument("--training_flag", type=bool, default=False, help="do training or not")
 parser.add_argument("--loading_id", type=str, default="", help="id of the model to load")
-parser.add_argument("--po_flag", type=bool, default=False, help="id of the model to load")
 opt = parser.parse_args()
-print(opt)
+
 
 if opt.model_name == "Oscillator":
     opt.species_labels = ["A", "B", "C"]
@@ -51,92 +49,67 @@ if opt.model_name == "SIR":
     opt.species_labels = ["S", "I", "R"]
 if opt.model_name == "ToggleSwitch":
     opt.species_labels = ["P1", "P2"]
+if opt.model_name == "MAPK":
+    opt.species_labels = ["V1", "MAPK-PP"]
+print(opt)
 
-
-opt.y_dim = opt.x_dim
 cuda = True if torch.cuda.is_available() else False
 #cuda = False
 
 model_name = opt.model_name
-PO_FLAG = opt.po_flag
-if PO_FLAG:
-    trainset_fn = "../Dataset_Generation/data/"+model_name+"/"+model_name+"_PO_dataset_fixed_param_1000x50.pickle"
-    test_fn = "../Dataset_Generation/data/"+model_name+"/"+model_name+"_PO_dataset_fixed_param_25x2000.pickle"
-else:
-    trainset_fn = "../Dataset_Generation/data/"+model_name+"/"+model_name+"_training_set.pickle"
-    test_fn = "../Dataset_Generation/data/"+model_name+"/"+model_name+"_validation_set.pickle"
+trainset_fn = "../Dataset_Generation/data/"+model_name+"/"+model_name+"_PO_training_set.pickle"
+testset_fn = "../Dataset_Generation/data/"+model_name+"/"+model_name+"_PO_validation_set.pickle"
+gridset_fn = "../Dataset_Generation/data/"+model_name+"/"+model_name+"_PO_grid_validation_set.pickle"
 
-ds = Dataset(trainset_fn, test_fn, opt.x_dim, opt.y_dim, opt.traj_len)
+ds = ParamDataset(trainset_fn, testset_fn, opt.x_dim, opt.par_dim, opt.traj_len)
 ds.load_train_data()
+ds.load_test_data()
+#ds.add_grid_data(gridset_fn)
+#ds.load_grid_test_data()
 
+opt.y_dim = opt.x_dim+opt.par_dim
 
 class Generator(nn.Module):
     def __init__(self):
         super(Generator, self).__init__()
 
         self.init_size = opt.traj_len // int(opt.traj_len/2)
-        self.l1 = nn.Sequential(nn.Linear(opt.latent_dim + opt.y_dim, 64 * self.init_size))
-
-        self.padd = 2
+        
+        self.padd = 1
         self.n_filters = 2*self.padd+1
+        self.Q = 2
+        self.Nch = 512
+        
+        self.l1 = nn.Sequential(nn.Linear(opt.latent_dim, self.Nch * self.Q))
+        
+        self.conv_blocks = nn.Sequential(
+            nn.ConvTranspose1d(self.Nch+opt.y_dim, 128, 4, stride=2, padding=self.padd),
+            nn.BatchNorm1d(128, 0.8),
+            nn.LeakyReLU(0.2, inplace=True),
+            
+            nn.ConvTranspose1d(128, 256, 4, stride=2, padding=self.padd),
+            nn.BatchNorm1d(256, 0.8),
+            nn.LeakyReLU(0.2, inplace=True),
 
-        if opt.traj_len == 32:
-            self.conv_blocks = nn.Sequential(
-                nn.BatchNorm1d(64),
-                nn.Upsample(scale_factor=2),
-                nn.Conv1d(64, 128, self.n_filters, stride=1, padding=self.padd),
-                nn.BatchNorm1d(128, 0.8),
-                nn.LeakyReLU(0.2, inplace=True),
-                nn.Upsample(scale_factor=2),
-                nn.Conv1d(128, 256, self.n_filters, stride=1, padding=self.padd),
-                nn.BatchNorm1d(256, 0.8),
-                nn.LeakyReLU(0.2, inplace=True),
-                nn.Upsample(scale_factor=2),
-                nn.Conv1d(256, 256, self.n_filters, stride=1, padding=self.padd),
-                nn.BatchNorm1d(256, 0.8),
-                nn.LeakyReLU(0.2, inplace=True),
-                nn.Conv1d(256, 128, self.n_filters, stride=1, padding=self.padd),
-                nn.BatchNorm1d(128, 0.8),
-                nn.LeakyReLU(0.2, inplace=True),
-                nn.Upsample(scale_factor=2),
-                nn.Conv1d(128, 64, self.n_filters, stride=1, padding=self.padd),
-                nn.BatchNorm1d(64, 0.8),
-                nn.LeakyReLU(0.2, inplace=True),
-                nn.Conv1d(64, opt.x_dim, self.n_filters, stride=1, padding=self.padd),
-                nn.Tanh(),
-            )
-        else:
-            self.conv_blocks = nn.Sequential(
-                nn.BatchNorm1d(64),
-                #nn.Upsample(scale_factor=2),
-                nn.Conv1d(64, 128, self.n_filters, stride=1, padding=self.padd),
-                nn.BatchNorm1d(128, 0.8),
-                nn.LeakyReLU(0.2, inplace=True),
-                nn.Upsample(scale_factor=2),
-                nn.Conv1d(128, 256, self.n_filters, stride=1, padding=self.padd),
-                nn.BatchNorm1d(256, 0.8),
-                nn.LeakyReLU(0.2, inplace=True),
-                nn.Upsample(scale_factor=2),
-                nn.Conv1d(256, 256, self.n_filters, stride=1, padding=self.padd),
-                nn.BatchNorm1d(256, 0.8),
-                nn.LeakyReLU(0.2, inplace=True),
-                nn.Conv1d(256, 128, self.n_filters, stride=1, padding=self.padd),
-                nn.BatchNorm1d(128, 0.8),
-                nn.LeakyReLU(0.2, inplace=True),
-                nn.Upsample(scale_factor=2),
-                nn.Conv1d(128, 64, self.n_filters, stride=1, padding=self.padd),
-                nn.BatchNorm1d(64, 0.8),
-                nn.LeakyReLU(0.2, inplace=True),
-                nn.Conv1d(64, opt.x_dim, self.n_filters, stride=1, padding=self.padd),
-                nn.Tanh(),
-            )
+            nn.ConvTranspose1d(256, 256, 4, stride=2, padding=self.padd),
+            nn.BatchNorm1d(256, 0.8),
+            nn.LeakyReLU(0.2, inplace=True),
+
+            nn.ConvTranspose1d(256, 128, 4, stride=2, padding=self.padd),
+            nn.BatchNorm1d(128, 0.8),
+            nn.LeakyReLU(0.2, inplace=True),
+
+            nn.Conv1d(128, opt.x_dim, self.n_filters, stride=1, padding=self.padd),
+            nn.Tanh(),
+        )
 
     def forward(self, noise, conditions):
         conds_flat = conditions.view(conditions.shape[0],-1)
-        gen_input = torch.cat((conds_flat, noise), 1)
-        out = self.l1(gen_input)     
-        out = out.view(out.shape[0], 64, self.init_size)
-        traj = self.conv_blocks(out)
+        conds_rep = conds_flat.repeat(1,self.Q).view(conditions.shape[0], opt.y_dim, self.Q)
+        noise_out = self.l1(noise)
+        noise_out = noise_out.view(noise_out.shape[0], self.Nch, self.Q)
+        gen_input = torch.cat((conds_rep, noise_out), 1)
+        traj = self.conv_blocks(gen_input)
         return traj
 
 
@@ -144,37 +117,35 @@ class Discriminator(nn.Module):
     def __init__(self):
         super(Discriminator, self).__init__()
 
-        def discriminator_block(in_filters, out_filters, bn=False):
-            padd = 2
-            n_filters = 2*padd+1            
-            block = [nn.Conv1d(in_filters, out_filters, n_filters, stride=1, padding=padd), nn.LeakyReLU(0.2, inplace=True), nn.Dropout(0.2)]
-            block.append(nn.LayerNorm([out_filters, opt.traj_len+1]))
-            if bn:
-                block.append(nn.BatchNorm1d(out_filters, 0.8))
+        def discriminator_block(in_filters, out_filters, L):
+            padd = 1
+            n_filters = 2*padd+2
+            block = [nn.Conv1d(in_filters, out_filters, n_filters, stride=2, padding=padd), nn.LeakyReLU(0.2, inplace=True), nn.Dropout(0.2)]
+            block.append(nn.LayerNorm([out_filters, L]))
+
             return block
 
         self.model = nn.Sequential(
-            *discriminator_block(opt.x_dim, 16),
-            *discriminator_block(16, 32),
-            *discriminator_block(32, 64),
-            *discriminator_block(64, 128),
-            *discriminator_block(128, 256)
+            *discriminator_block(opt.x_dim, 64, (opt.traj_len+opt.y_dim)//2),
+            *discriminator_block(64, 64, ((opt.traj_len+opt.y_dim)//2)//2)
         )
 
         # The height and width of downsampled image
-        ds_size = opt.traj_len + 1
-        self.adv_layer = nn.Sequential(nn.Linear(256 * ds_size, 1))
+        ds_size = (opt.traj_len + opt.y_dim) // (2**2)
+        self.adv_layer = nn.Sequential(nn.Linear(64 * ds_size, 1))
         
     def forward(self, trajs, conditions):
+        #print("A------", trajs.shape, conditions.shape)
         d_in = torch.cat((conditions, trajs), 2)
+        
         out = self.model(d_in)
-        #summary(self.model, d_in)
+        #print("B------", out.shape)
         out_flat = out.view(out.shape[0], -1)
         validity = self.adv_layer(out_flat)
+        #print("C------", validity.shape)
         return validity
 
-
-DO_TRAINING = False
+DO_TRAINING = True
 
 if DO_TRAINING:
     ID = str(np.random.randint(0,100000))
@@ -182,7 +153,7 @@ if DO_TRAINING:
 else:
     ID = opt.loading_id
 
-plots_path = model_name+"/MA_Plots/ID_"+ID
+plots_path = model_name+"/MA_w_Plots/ID_"+ID
 os.makedirs(plots_path, exist_ok=True)
 f = open(plots_path+"/log.txt", "w")
 f.write(str(opt))
@@ -219,7 +190,7 @@ def compute_gradient_penalty(D, real_samples, fake_samples, lab):
     return gradient_penalty
 
 def generate_random_conditions():
-    return (np.random.rand(opt.batch_size, opt.y_dim, 1)-0.5)*2
+    return (np.random.rand(opt.batch_size, opt.x_dim, opt.y_dim)-0.5)*2
 
 # ----------
 #  Training
@@ -277,7 +248,6 @@ if DO_TRAINING:
             fake_trajs = generator(z, conds)
             # Real images
             real_validity = discriminator(real_trajs, conds)
-
             # Fake images
             fake_validity = discriminator(fake_trajs, conds)
             # Gradient penalty
@@ -322,8 +292,10 @@ if DO_TRAINING:
                 )
 
                 batches_done += opt.n_critic
-        if epoch+1 % 500 == 0:
-        	torch.save(generator, plots_path+"/generator_{}epochs.pt".format(epoch))    
+
+        if epoch+1 % 250 == 0:
+            torch.save(generator, plots_path+"/generator_{}epochs.pt".format(epoch))    
+        
         D_losses.append(np.mean(tmp_D_loss))
         G_losses.append(np.mean(tmp_G_loss))
     
@@ -365,117 +337,85 @@ else:
     generator.eval()
     if cuda:
         generator.cuda()
+
+print("Computing test trajectories...")
 ds.load_test_data()
-if False:
-    print("Computing test trajectories...")
-    
-    n_gen_trajs = ds.n_traj_per_point
-    gen_trajectories = np.empty(shape=(ds.n_points_test, n_gen_trajs, opt.x_dim, opt.traj_len))
-    for iii in range(ds.n_points_test):
-        print("Test point nb ", iii+1, " / ", ds.n_points_test)
-        for jjj in range(n_gen_trajs):
-            z_noise = np.random.normal(0, 1, (1, opt.latent_dim))
-            temp_out = generator(Variable(Tensor(z_noise)), Variable(Tensor([ds.Y_test_transp[iii]])))
-            gen_trajectories[iii,jjj] = temp_out.detach().cpu().numpy()[0]
+n_gen_trajs = ds.n_traj_per_point
+gen_trajectories = np.empty(shape=(ds.n_points_test, n_gen_trajs, opt.x_dim, opt.traj_len))
+for iii in range(ds.n_points_test):
+    print("Test point nb ", iii+1, " / ", ds.n_points_test)
+    for jjj in range(n_gen_trajs):
+        z_noise = np.random.normal(0, 1, (1, opt.latent_dim))
+        temp_out = generator(Variable(Tensor(z_noise)), Variable(Tensor([ds.Y_test_transp[iii]])))
+        gen_trajectories[iii,jjj] = temp_out.detach().cpu().numpy()[0]
 
-
-    trajs_dict = {"gen_trajectories": gen_trajectories}
-    file = open(plots_path+'/generated_validation_trajectories.pickle', 'wb')
-    # dump information to that file
-    pickle.dump(trajs_dict, file)
-    # close the file
-    file.close()
-else:
-    file = open(plots_path+'/generated_validation_trajectories.pickle', 'rb')
-    trajs_dict = pickle.load(file)
-    file.close()
-    gen_trajectories = trajs_dict["gen_trajectories"]
+trajs_dict = {"gen_trajectories": gen_trajectories}
+file = open(plots_path+'/generated_validation_trajectories.pickle', 'wb')
+# dump information to that file
+pickle.dump(trajs_dict, file)
+# close the file
+file.close()
 
 colors = ['blue', 'orange']
 leg = ['real', 'gen']
+#PLOT TRAJECTORIES 
+n_trajs_to_plot = 10
+print("Plotting test trajectories...")      
+tspan = range(opt.traj_len)
+for kkk in range(ds.n_points_test):
+    print("Test point nb ", kkk+1, " / ", ds.n_points_test)
+    fig, ax = plt.subplots(opt.x_dim)
+    G = np.array([np.round(ds.HMIN+(gen_trajectories[kkk, it].T+1)*(ds.HMAX-ds.HMIN)/2).T for it in range(ds.n_traj_per_point)])
+    R = np.array([np.round(ds.HMIN+(ds.X_test_transp[kkk, it].T+1)*(ds.HMAX-ds.HMIN)/2).T for it in range(ds.n_traj_per_point)])
 
-#PLOT TRAJECTORIES
-if False: #scaled trajs
-    n_trajs_to_plot = 10
-    print("Plotting test trajectories...")      
-    tspan = range(opt.traj_len)
-    for kkk in range(ds.n_points_test):
-        print("Test point nb ", kkk+1, " / ", ds.n_points_test)
-        fig, axs = plt.subplots(opt.x_dim)
-
-        for d in range(opt.x_dim):
-            
-            for traj_idx in range(n_trajs_to_plot):
-                axs[d].plot(tspan, ds.X_test_transp[kkk,traj_idx, d], color=colors[0])
-                axs[d].plot(tspan, gen_trajectories[kkk,traj_idx,d], color=colors[1])
-                
-        plt.tight_layout()
-        fig.savefig(plots_path+"/"+opt.model_name+"_Trajectories"+str(kkk)+".png")
-        plt.close()
-else: # trajs in original (integer) scale
-    n_trajs_to_plot = 10
-    print("Plotting test trajectories...")  
-    
-    tspan = range(opt.traj_len)
-    for kkk in range(ds.n_points_test):
-        print("Test point nb ", kkk+1, " / ", ds.n_points_test)
-        fig, axs = plt.subplots(opt.x_dim)
-        G = np.array([np.round(ds.HMIN+(gen_trajectories[kkk, it].T+1)*(ds.HMAX-ds.HMIN)/2).T for it in range(ds.n_traj_per_point)])
-        R = np.array([np.round(ds.HMIN+(ds.X_test_transp[kkk, it].T+1)*(ds.HMAX-ds.HMIN)/2).T for it in range(ds.n_traj_per_point)])
-        for d in range(opt.x_dim):
-            
-            for traj_idx in range(n_trajs_to_plot):
-
-                axs[d].plot(tspan, R[traj_idx, d], color=colors[0])
-                axs[d].plot(tspan, G[traj_idx,d], color=colors[1])
-                
-        plt.tight_layout()
-        fig.savefig(plots_path+"/"+opt.model_name+"_Rescaled_Trajectories"+str(kkk)+".png")
-        plt.close()
-
+    for d in range(opt.x_dim):
+        if opt.x_dim == 1:
+            axd = ax
+        else:
+            axd = ax[d]
+        for traj_idx in range(n_trajs_to_plot):
+            #axd.plot(tspan, ds.X_test_transp[kkk,traj_idx, d], color=colors[0])
+            #axd.plot(tspan, gen_trajectories[kkk,traj_idx,d], color=colors[1])
+            axd.plot(tspan, R[traj_idx, d], color=colors[0])
+            axd.plot(tspan, G[traj_idx,d], color=colors[1])
+    plt.tight_layout()
+    #fig.savefig(plots_path+"/"+opt.model_name+"_Trajectories"+str(kkk)+".png")
+    fig.savefig(plots_path+"/"+opt.model_name+"_Rescaled_Trajectories"+str(kkk)+".png")
+    plt.close()
 
 #PLOT HISTOGRAMS
-if False:
+if True:
+
     bins = 50
     time_instant = -1
     print("Plotting histograms...")
     for kkk in range(ds.n_points_test):
         fig, ax = plt.subplots(opt.x_dim,1, figsize = (12,opt.x_dim*3))
         for d in range(opt.x_dim):
-            XXX = np.vstack((ds.X_test_transp[kkk,:,d, time_instant], gen_trajectories[kkk,:,d, time_instant])).T
-            
-            ax[d].hist(XXX, bins = bins, stacked=False, density=False, color=colors, label=leg)
-            ax[d].legend()
-            ax[d].set_ylabel(opt.species_labels[d])
-
-        figname = plots_path+"/"+opt.model_name+"_hist_comparison_{}th_timestep_{}.png".format(time_instant, kkk)
-        fig.savefig(figname)
-
-        plt.close()
-else:
-    bins = 50
-    time_instant = -1
-    print("Plotting histograms...")
-    for kkk in range(ds.n_points_test):
-        fig, ax = plt.subplots(opt.x_dim,1, figsize = (12,opt.x_dim*3))
-        for d in range(opt.x_dim):
+            #XXX = np.vstack((ds.X_test_transp[kkk,:,d, time_instant], gen_trajectories[kkk,:,d, time_instant])).T
             G = np.array([np.round(ds.HMIN+(gen_trajectories[kkk, it].T+1)*(ds.HMAX-ds.HMIN)/2).T for it in range(ds.n_traj_per_point)])
             R = np.array([np.round(ds.HMIN+(ds.X_test_transp[kkk, it].T+1)*(ds.HMAX-ds.HMIN)/2).T for it in range(ds.n_traj_per_point)])
-        
+
             XXX = np.vstack((R[:,d, time_instant], G[:,d, time_instant])).T
             
-            ax[d].hist(XXX, bins = bins, stacked=False, density=False, color=colors, label=leg)
-            ax[d].legend()
-            ax[d].set_ylabel(opt.species_labels[d])
+            if opt.x_dim == 1:
+                axd = ax
+            else:
+                axd = ax[d]
+            axd.hist(XXX, bins = bins, stacked=False, density=False, color=colors, label=leg)
+            axd.legend()
+            axd.set_ylabel(opt.species_labels[d])
 
+        #figname = plots_path+"/"+opt.model_name+"_hist_comparison_{}th_timestep_{}.png".format(time_instant, kkk)
         figname = plots_path+"/"+opt.model_name+"_rescaled_hist_comparison_{}th_timestep_{}.png".format(time_instant, kkk)
         fig.savefig(figname)
 
         plt.close()
 
+
 #COMPUTE WASSERSTEIN DISTANCES
 
-if False:
+if True:
     dist = np.zeros(shape=(ds.n_points_test, opt.x_dim, opt.traj_len))
     print("Computing and Plotting Wasserstein distances...") 
     for kkk in range(ds.n_points_test):
